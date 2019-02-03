25ec3518fcbfc5628d0343f3404f1c64570b0732
cecil john tantay <cjbtantay@gmail.com>
2019-02-03 14:50:01 -0800

    set up boilerplate for redux and filestructure

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppWithRouter = withRouter(App);
+
+const app = (
+  <Provider store={store}>
+    <Router>
+      <AppWithRouter />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
