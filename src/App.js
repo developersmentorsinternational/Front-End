1a5c6d587f0881a3245e32f5e05d2f4cca0c3ef9
cecil john tantay <cjbtantay@gmail.com>
2019-02-05 12:04:50 -0800

    remove HOC for login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import SignupView from './view/SignupView';
-import Authorize from './components/Authorization/Authorize';
 import DashboardView from './view/DashboardView';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class App extends React.Component {
   componentDidMount() {
-    console.log('hello: ', this.props.isLogginSuccess);
+    if (this.props.isLogginSuccess || this.props.isRegisterSuccess) {
+      this.props.history.push('/dashboard');
+    } else {
+      this.props.history.push('/signup');
+    }
+
     if (localStorage.getItem('data')) {
       this.props.history.push('/dashboard');
     }
@@ -18,25 +22,12 @@ class App extends React.Component {
 
     return (
       <>
-        <Route
-          exact
-          path='/'
-          render={props => (
-            <Authorization
-              {...props}
-              isLogginSuccess={this.props.isLogginSuccess}
-              isRegisterSuccess={this.props.isRegisterSuccess}
-            />
-          )}
-        />
         <Route path='/dashboard' component={DashboardView} />
         <Route path='/signup' component={SignupView} />
       </>
     );
   }
 }
-
-const Authorization = Authorize(DashboardView)(SignupView);
 
 const mapStateToProps = state => ({
   isLogginSuccess: state.auth.isLogginSuccess,
