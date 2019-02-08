5808b7d5874d63108f718df88201e15d47dc07be
cecil john tantay <cjbtantay@gmail.com>
2019-02-08 07:12:57 -0800

    dashboard

--- src/components/Dashboard/SchedulesList/ScheduleList.js
+++ src/components/Dashboard/SchedulesList/ScheduleList.js
@@ -2,18 +2,28 @@ import React from 'react';
 import { Paper, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = () => ({
+const styles = theme => ({
   root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+
+    maxWidth: '1000px',
     width: '100%'
   }
 });
 
 const ScheduleList = props => {
-  const { classes } = props;
+  const { classes, schedule } = props;
   return (
     <div className={classes.root}>
-      <Paper>
-        <Typography>SCHEDULES</Typography>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant='h5' component='h3'>
+          {schedule.name}
+        </Typography>
+        <Typography component='p'>{schedule.start}</Typography>
+
+        <Typography component='p'>{schedule.created}</Typography>
       </Paper>
     </div>
   );
