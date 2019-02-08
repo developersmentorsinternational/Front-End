608558181bede957ce1e499a2d307f25004dd56e
olympiawojcik <owojcik7@gmail.com>
2019-02-07 20:04:07 -0500

    change background color of dashboard and message form button

--- src/components/MessagePage/MessageForm.js
+++ src/components/MessagePage/MessageForm.js
@@ -1,41 +1,49 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Icon from '@material-ui/core/Icon';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import TagFacesIcon from '@material-ui/icons/TagFaces';
-import Chip from '@material-ui/core/Chip';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Icon from "@material-ui/core/Icon";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import TagFacesIcon from "@material-ui/icons/TagFaces";
+import Chip from "@material-ui/core/Chip";
 
 const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
-    maxWidth: '500px',
-    width: '100%',
-    margin: '0 auto'
+    maxWidth: "1000px",
+    width: "100%",
+    margin: "0 auto"
   },
   chip: {
     margin: theme.spacing.unit / 2
+  },
+
+  button: {
+    backgroundColor: "#00AFC1"
+  },
+
+  reset: {
+    backgroundColor: "white"
   }
 });
 
 class MessageForm extends React.Component {
   state = {
     chipData: [
-      { key: 0, label: 'Angular' },
-      { key: 1, label: 'jQuery' },
-      { key: 2, label: 'Polymer' },
-      { key: 3, label: 'React' },
-      { key: 4, label: 'Vue.js' }
+      { key: 0, label: "Angular" },
+      { key: 1, label: "jQuery" },
+      { key: 2, label: "Polymer" },
+      { key: 3, label: "React" },
+      { key: 4, label: "Vue.js" }
     ]
   };
 
   handleDelete = data => () => {
-    if (data.label === 'React') {
-      alert('Why would you want to delete React?! :)'); // eslint-disable-line no-alert
+    if (data.label === "React") {
+      alert("Why would you want to delete React?! :)"); // eslint-disable-line no-alert
       return;
     }
 
@@ -51,27 +59,30 @@ class MessageForm extends React.Component {
     const { classes } = this.props;
     return (
       <div>
-        <Typography variant='h5' component='h1'>
+        <Typography variant="h5" component="h1">
           Send a Message
         </Typography>
+
         <Paper className={classes.root} elevation={1}>
-          <Typography variant='h5' component='h3'>
+          <Typography variant="h5" component="h3">
             Who do you want to send it to?
           </Typography>
-          <Typography component='p'>Select a contact:</Typography>
+
+          <Typography component="p">Select a contact:</Typography>
+
           <TextField
-            id='standard-search'
-            label='Search field'
-            type='search'
+            id="standard-search"
+            label="Search field"
+            type="search"
             className={classes.textField}
-            margin='normal'
+            margin="normal"
             required
           />
 
           {this.state.chipData.map(data => {
             let icon = null;
 
-            if (data.label === 'React') {
+            if (data.label === "React") {
               icon = <TagFacesIcon />;
             }
 
@@ -85,38 +96,42 @@ class MessageForm extends React.Component {
               />
             );
           })}
-          <form className={classes.container} noValidate autoComplete='off'>
+          <form className={classes.container} noValidate autoComplete="off">
             <TextField
               required
-              id='outlined-required'
-              label='Subject'
+              id="outlined-required"
+              label="Subject"
               className={classes.textField}
-              margin='normal'
-              variant='outlined'
+              margin="normal"
+              variant="outlined"
               fullWidth
             />
             <TextField
-              id='outlined-multiline-static'
-              label='Message'
+              id="outlined-multiline-static"
+              label="Message"
               multiline
-              rows='4'
+              rows="4"
               className={classes.textField}
-              margin='normal'
-              variant='outlined'
+              margin="normal"
+              variant="outlined"
               fullWidth
               required
             />
             <Button
-              type='submit'
-              variant='contained'
-              color='primary'
+              type="submit"
+              variant="contained"
+              color="primary"
               className={classes.button}
             >
               Send
               {/* This Button uses a Font Icon, see the installation instructions in the docs. */}
               <Icon className={classes.rightIcon}>send</Icon>
             </Button>
-            <Button variant='contained' className={classes.button}>
+            <Button
+              variant="contained"
+              className={classes.button}
+              className={classes.reset}
+            >
               Reset
             </Button>
           </form>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './reducers';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 const middlewares = [thunk, logger];
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middlewares))
 );
 
 export default store;
